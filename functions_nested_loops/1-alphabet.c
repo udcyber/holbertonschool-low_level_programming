12d690b246da14b1306722ed7711469948e8ec0a
udcyber <udewaleyne.pro@gmail.com>
2026-02-25 14:09:49 +0000

    add alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print alphabet
+ * in lowercase
+ * followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+        int c;
+
+        c = 'a';
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                _putchar (c);
+        }
+        _putchar ('\n');
+        return (0);
+}
