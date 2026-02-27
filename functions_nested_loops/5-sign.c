53494466ae48545b71a4768a6ce7d6c0662a4fac
udcyber <udewaleyne.pro@gmail.com>
2026-02-27 12:39:16 +0000

    print + for 1, 0 for 0, -1 for -

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,19 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		print_sign('+');
-		print_sign(1);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		print_sign(0);
+		_putchar(0);
 		return (0);
 	}
 	else if (n < 0)
 	{
-		print_sign('-');
-		print_sign(1);
+		_putchar('-');
 		return (-1);
 	}
 	return (0);
