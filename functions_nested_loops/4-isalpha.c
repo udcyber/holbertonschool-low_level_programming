15774653229c3b31fbe54bfdc8ff5761ed0c1c3e
udcyber <udewaleyne.pro@gmail.com>
2026-02-27 10:05:23 +0000

    check lowercase or uppercase give 1 if not 0

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ *
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
