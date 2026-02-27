699e585dc3d68400bd2aa595186eed0730f163e4
udcyber <udewaleyne.pro@gmail.com>
2026-02-27 14:05:13 +0000

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @c: print last digit of number
+ *
+ * Return: 0
+ */
+
+int print_last_digit(int c)
+{
+
+	int LastDigit = c % 10;
+
+	_putchar(LastDigit);
+
+	return (LastDigit);
+}
