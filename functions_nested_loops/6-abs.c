78ccdd5d2a9e7e429218949a1cb15021c556b943
udcyber <udewaleyne.pro@gmail.com>
2026-02-27 13:36:51 +0000

    compute absolut value of integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @n: compute absolute value
+ * of an integer
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (0);
+}
+
