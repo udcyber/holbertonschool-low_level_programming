7a7b9833dda67bfa301bd5b4196d9a18b8375fa1
udcyber <udewaleyne.pro@gmail.com>
2026-02-28 21:18:16 +0000

    simple loop and verify with pytho tutor

---
+++ intro_debugging/sum_to_n.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum_to_n(int n)
+{
+    int i;
+    int sum = 0;
+
+    for (i = 1; i <= n; i++)
+        sum += i;
+
+    return n;
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+    printf("%d\n", sum_to_n(10));
+    return 0;
+}
