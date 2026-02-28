3bb7ec364d443d90898aad3fa972d32a75e2ae30
udcyber <udewaleyne.pro@gmail.com>
2026-02-28 18:39:21 +0000

    print a full day clock

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * print a full day clock
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int fenzhong, xiaoshi;
+
+	fenzhong = 0;
+	xiaoshi = 0;
+
+	while (xiaoshi <= 23)
+	{
+		while (fenzhong <= 59)
+		{
+			if (xiaoshi < 10)
+			{
+				_putchar(0);
+			}
+			_putchar(xiaoshi);
+			_putchar(':');
+			if (fenzhong < 10)
+			{
+				_putchar(0);
+			}
+			_putchar(fenzhong);
+			_putchar('\n');
+			fenzhong++;
+		}
+		xiaoshi++;
+		fenzhong = 0;
+	}
+}
