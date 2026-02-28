e3bc3f0c665b367e950ce7a5a2dcc97ac940982a
udcyber <udewaleyne.pro@gmail.com>
2026-02-28 19:54:31 +0000

    add two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @a: first integer
+ * @b: second integer
+ * add 2 integers and return result
+ *
+ * Return: 0
+ */
+
+int add(a, b);
+{
+	add(a, b)
+
+	return (a + b);
+}
