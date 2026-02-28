0cc94a3eeb4eb57212ea957af5b1105403d312ea
udcyber <udewaleyne.pro@gmail.com>
2026-02-28 20:42:02 +0000

    print all natural numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,17 @@
 
 void print_to_98(int n)
 {
-	while (n < 98)
+	if (n >= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
 	}
-	while (n > 98)
+	else
 	{
-		printf("%d, ", n);
-		n++;
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-	_putchar('\n');
 }
