94217a4a44c593e04932a30f38e91c9a9ccdb4df
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 07:31:47 +0000

    print 1 to 9 followed by new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * print 0 to 9
+ * followed by new line
+ * @a = variable
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
