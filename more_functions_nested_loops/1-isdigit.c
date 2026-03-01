c8d4f6cfbf6334a9c1a168af7891a956a8067e05
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 07:08:28 +0000

    check for digit from 0 to 9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -9,20 +9,18 @@
 
 int _isdigit(int c)
 {
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+
 	if (_isdigit(c))
 	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
 		_putchar(1);
 		_putchar('\n');
 		return (1);
 	}
 	else
 	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
 		_putchar(0);
 		_putchar('\n');
 		return (0);
