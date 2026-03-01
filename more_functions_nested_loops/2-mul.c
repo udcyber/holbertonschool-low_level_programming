6c8a33af034914b430b57b5bac742184ba26a8ec
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 07:17:26 +0000

    multiply a by b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	_putchar(a * b);
+	return (0);
+}
