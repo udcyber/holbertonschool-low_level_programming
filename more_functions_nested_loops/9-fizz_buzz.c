052707554174c1b9d8b4a62a7071d1ea88ed1ae0
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 17:37:40 +0000

    fizz buzz test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -22,21 +22,13 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3)
-		{
 			printf("Fizz ");
-		}
 		else if (n % 5)
-		{
 			printf("Buzz ");
-		}
 		else if (n % 3 && n % 5)
-		{
 			printf("FizzBuzz ");
-		}
 		else
-		{
 			printf("%c ", n);
-		}
 	}
 	printf("\n");
 
