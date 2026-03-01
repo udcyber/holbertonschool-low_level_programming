b8d88cda0f7e82c518a7ec08226fec9775cb45a7
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 13:52:36 +0000

    draw square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * print a square
+ * @size: size of square
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
