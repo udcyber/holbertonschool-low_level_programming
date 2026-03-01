4834b61b0604017c5b1ad8b843fe3f1f59077a84
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 08:29:15 +0000

    print 10 times the numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,14 @@
 
 void more_numbers(void)
 {
-	int a, b;
+	int a;
+	char b;
 
 	for (a = 0; a <= 10; a++)
 	{
 		for (b = 0; b <= 14 ; b++)
 		{
-			_putchar(b + '0');
+			_putchar(b);
 		}
 		_putchar('\n');
 	}
