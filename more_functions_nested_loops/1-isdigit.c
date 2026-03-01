dcfd3bef0a2271a93df7e5c3ecfb45691dc93d93
udcyber <udewaleyne.pro@gmail.com>
2026-03-01 06:43:23 +0000

    check for digit from 0 to 9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,20 @@
 /**
  * _isdigit - entry point
  * check for digit 0 through 9
- *
+ * @c: variable
  * Return: 1 or 0
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	_isdigit();
+	if _isdigit(c)
+	{
+		_putchar("%c: %d\n", c, _isdigit(c));
+		return (1);
+	}
+	else
+	{
+		_putchar("%c: %d\n", c, _isdigit(c));
+		return (0);
+	}
 }
