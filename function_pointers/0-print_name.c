4782bfd33355ba6e34aa76c116cbba532703d99c
udcyber <udewaleyne.pro@gmail.com>
2026-03-20 05:47:37 +0100

    task 0. PRINT name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,18 +1,18 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdlib.h>
 
 /**
  * print_name - print a name
  * @name: the name to print
- * @f: pointer to a function that print a name
- * Return: NULL if void
+ * @f: pointer to a function that prints a name
+ * Return: nothing if void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 	{
-		return (NULL);
+		;
 	}
 	f(name);
 }
