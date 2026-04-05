50f925f530db9693673a909048ccf7c21b9c0555
udcyber <udewaleyne.pro@gmail.com>
2026-04-05 21:52:53 +0200

    implement algo

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implement djb2 algorithm
+ * @str: string
+ * Return: hash
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int x;
+
+	hash = 5381;
+
+	while ((x = *str++))
+	{
+		hash = ((hash << 5) + hash) + x;
+	}
+
+	return (hash);
+}
